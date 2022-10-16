e54882bd133514bbf85842d2c5f5cebf026deb68
sabrina663 <bina.oliver4@gmail.com>
2022-10-15 22:36:10 -0300

    o serviço de hospedagem não consegue se conectar com o mongodb

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require("express");
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 const app = express();
-const caracters = require("./caracters");
+const caracters = require("./caracters.json");
 const port = process.env.PORT || 8080;
 
-
-mongoose.connect('mongodb+srv://root123:root123@cluster0.jkoel2y.mongodb.net/asylum?retryWrites=true&w=majority').
-    then(console.log("Connect ok")).catch(e => console.log("err:" +  e.message));
+//A plataforma não permite se conecta com o mongodb, então usei um file.json
+// mongoose.connect('mongodb+srv://root123:root123@cluster0.jkoel2y.mongodb.net/asylum?retryWrites=true&w=majority').
+//     then(console.log("Connect ok")).catch(e => console.log("err:" +  e.message));
 
 app.get("/", (req, res) => {
-    caracters.find({}).exec((err, caracter) => {
-        res.send(caracter)
-    })
+    res.send(caracters)
 });
 
 
